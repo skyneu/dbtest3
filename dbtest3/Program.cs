30957a5c44acc27dc4acce5306110b59952bc555
YumeiLi <yumeili@tamu.edu>
2020-01-24 12:13:51 -0600

    writing method done

--- dbtest3/Program.cs
+++ dbtest3/Program.cs
@@ -9,19 +9,58 @@ namespace dbtest3
 {
     class Program
     {
+        static MongoClient client = null;
+        static IMongoDatabase database = null;
+        static IMongoCollection<BsonDocument> collection = null;
+
         static void Main(string[] args)
         {
-            CallMain(args).Wait();
-            Console.ReadLine();
+            SetupDatabase();
+            LoadData().Wait();
+            while (true)
+            {
+                Console.Write("> ");
+                string line = Console.ReadLine();
+                if (line.ToLower() == "load" || line.ToLower() == "read")
+                {
+                    LoadData().Wait();
+                } else if (line.ToLower() == "exit" || line.ToLower() == "quit")
+                {
+                    break;
+                } else
+                {
+                    BsonDocument document;
+                    try
+                    {
+                        document = BsonDocument.Parse(line);
+                    }
+                    catch(FormatException e)
+                    {
+                        Console.WriteLine("Invalid JSON, please try again.");
+                        continue;
+                    }
+
+                    collection.InsertOne(document);
+                    Console.WriteLine("Document inserted");
+                }
+            }
         }
-        static async Task CallMain(string[] args)
+
+        static void SetupDatabase()
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Setup Database Hello World");
 
-            var client = new MongoClient("mongodb+srv://consumerteam:01292020@cluster0-tkvaq.azure.mongodb.net/test?retryWrites=true&w=majority");
-            var database = client.GetDatabase("datatest1");
+            if (client == null)
+            {
+                client = new MongoClient("mongodb+srv://consumerteam:01292020@cluster0-tkvaq.azure.mongodb.net/test?retryWrites=true&w=majority");
+                database = client.GetDatabase("datatest1");
+                collection = database.GetCollection<BsonDocument>("dbt1");
+            }
 
-            var collection = database.GetCollection<BsonDocument>("dbt1");
+        }
+
+        static async Task LoadData()
+        {
 
             //Method 1
             using (var cursor = await collection.Find(new BsonDocument()).ToCursorAsync())
@@ -34,8 +73,8 @@ namespace dbtest3
                     }
                 }
             }
-
         }
+
     }
 }
 
